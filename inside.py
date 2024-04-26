e8c548aec2ce75257095f2ebd0ea817cfcfca160
Dulmina Peiris <80464245+dulminapeiris@users.noreply.github.com>
2024-04-26 20:48:57 +0530

    Update inside.py

--- inside.py
+++ inside.py
@@ -1,6 +1,5 @@
 from googlesearch import search
 
-# ASCII art for the tool name
 xxxxxxxxxxxxx = """
 .__              .__    .___        ____ _____________.____     
 |__| ____   _____|__| __| _/____   |    |   \______   \    |    
@@ -17,10 +16,9 @@ def search_urls_with_keyword(keyword, site, num_results):
     return urls
 
 def main():
-    print("\033[1m")  # bold text
-    print("\033[93m" + xxxxxxxxxxxxx + "\033[0m")  # yellow color for the ASCII art
-    print("\033[0m")  # reset text formatting
-    
+    print("\033[1m")
+    print("\033[93m" + xxxxxxxxxxxxx + "\033[0m")  
+    print("\033[0m") 
     site = input("Enter the website (e.g., youtube.com): ")
     keyword = input("Enter the keyword to search for in the URL: ")
     num_results = int(input("Enter the number of results to fetch: "))
@@ -32,7 +30,7 @@ def main():
     for url in urls:
         print(url)
     
-    print("\n\033[2mA proud open source security project by Sajiya\033[0m")  # dim text for credits
+    print("\n\033[2mA proud open source security project by Sajiya\033[0m") 
 
 if __name__ == "__main__":
     main()
